ec92df51c8aec22b3d7b06876e329b2db05f7f3b
EndureBlaze <hi@imyan.ren>
2022-06-27 17:58:42 +0800

    ✨ Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,32 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import eslintPlugin from 'vite-plugin-eslint'
 import postCssPxToRem from 'postcss-pxtorem'
+import autoprefixer from 'autoprefixer'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
+      dts: 'src/auto-import.d.ts',
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+
+      dts: 'src/components.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
@@ -15,15 +37,23 @@ export default defineConfig({
     },
   },
   base: './',
-  server: {
-    port: 3000,
-  },
   define: {
     'process.env.BASE_URL': '"/"',
   },
   css: {
     postcss: {
       plugins: [
+        autoprefixer({
+          overrideBrowserslist: [
+            'Android 4.1',
+            'iOS 7.1',
+            'Chrome > 31',
+            'ff > 31',
+            'ie >= 8',
+            '> 1%',
+          ],
+          grid: true,
+        }),
         postCssPxToRem({
           rootValue: 100,
           propList: ['*'],
